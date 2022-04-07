0079ebc5e1a1b2343b0c5bfd6b8fd4fbc2c66c89
show kawazoe <show753@icloud.com>
2022-04-07 16:18:01 +0900

    add sass

---
+++ src/components/Mv.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import * as Style from './Mv.module.scss'
+
+const Mv = () => {
+  console.log(Style)
+  return (
+    <div className={Style.wrap}>
+      <Container className={Style.content}>
+        <Row>
+          <Col sm={12} className="mx-auto">
+            <div className={Style.siteHeading}>
+              <h1>Gatsby blog</h1>
+              <span>A Blog by Gatsby.js</span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Mv
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
+import Mv from "../components/Mv"
 
 const IndexPage = () => (
   <Layout>
-    <h1>index</h1>
+    <Mv />
   </Layout>
 )
 
