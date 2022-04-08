70e48586c561f9cf7dc7019ad3081e2bca144fe8
show kawazoe <show753@icloud.com>
2022-04-08 16:16:58 +0900

    add blog detail query

--- src/templates/blogDetail.js
+++ src/templates/blogDetail.js
@@ -1,9 +1,32 @@
 import * as React from 'react'
+import {graphql} from 'gatsby'
 import Layout from '../components/layout'
+import { Container } from 'react-bootstrap'
 
-const BlogDetail = () => (
+ export const query = graphql`
+  query( $slug: String! ) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+const BlogDetail = (props) => (
   <Layout>
-    blog template
+    <Container style={{maxWidth:640}} className="pt-4">
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+    </Container>
   </Layout>
 )
 
