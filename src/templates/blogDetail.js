80e895babd2690ab8e9b96913c7412da191c3500
show kawazoe <show753@icloud.com>
2022-04-08 19:51:28 +0900

    add renderRichText

--- src/templates/blogDetail.js
+++ src/templates/blogDetail.js
@@ -2,30 +2,25 @@ import * as React from 'react'
 import {graphql} from 'gatsby'
 import Layout from '../components/layout'
 import { Container } from 'react-bootstrap'
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
- export const query = graphql`
-  query( $slug: String! ) {
-    markdownRemark (
-      fields: {
-        slug: {
-          eq: $slug
-        }
+export const query = graphql`
+  query($slug: String) {
+    contentfulBlogPost(slug: {eq: $slug}) {
+      title
+      createdDate
+      body {
+        raw
       }
-    ) {
-      frontmatter {
-        title
-        date
-      }
-      html
     }
   }
 `
 const BlogDetail = (props) => (
   <Layout>
     <Container style={{maxWidth:640}} className="pt-4">
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.createdDate}</p>
+      { props.data.contentfulBlogPost.body && renderRichText(props.data.contentfulBlogPost.body) }
     </Container>
   </Layout>
 )
